96ca904b084dae78cccea7e39c1c741c0dc56af8
SefaSAHAN <sefasahan35@gmail.com>
2023-01-15 22:17:37 +0100

    without email validator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from email.mime.text import MIMEText
 import smtplib
-from validate_email import validate_email
+
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
@@ -12,28 +12,25 @@ def index():
         name= request.form["name"]
         email=request.form["email"]
         text=request.form["message"]
-        if validate_email(email):
-            sender_email = "pycodersfenyx@gmail.com"
-            receiver_email = "sefasahan35@gmail.com"
-            password = "rvpqrzownkvhyvqz"
-            subject = "This is the subject of the email"
-            msg = MIMEText(f"Name: {name}\nEmail: {email}\nText: {text}")
-            msg['Subject'] = subject
-            msg['From'] = sender_email
-            msg['To'] = receiver_email
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(sender_email, password)
-            try:
-                server.sendmail(sender_email, receiver_email, msg.as_string())
-                print("email sent successfully")
-            except Exception as e:
-                print("error occured:", e)
-            server.quit()
-            return render_template("index.html", name=name, email=email, text=text)
-        else:
-            error_msg = "Invalid email address"
-            return render_template("index.html", name=name, email=email, text=text, error_msg=error_msg)
+        sender_email = "pycodersfenyx@gmail.com"
+        receiver_email = "sefasahan35@gmail.com"
+        password = "rvpqrzownkvhyvqz"
+        subject = "This is the subject of the email"
+        msg = MIMEText(f"Name: {name}\nEmail: {email}\nText: {text}")
+        msg['Subject'] = subject
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, password)
+        try:
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+            print("email sent successfully")
+        except Exception as e:
+            print("error occured:", e)
+        server.quit()
+        return render_template("index.html", name=name, email=email, text=text)
+        
     return render_template("index.html")
 
 if __name__ == '__main__':
